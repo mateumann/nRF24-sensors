73a1a604c9abe6954d22067bacbb09c012d10677
Mateusz Neumann <mateumann@gmail.com>
2020-02-13 13:01:07 +0100

    Add BME280 sensor with example code

--- src/main.cpp
+++ src/main.cpp
@@ -4,144 +4,196 @@
 * Updated: Dec 2014 by TMRh20
 */
 
-#include "Arduino.h"
-#include "LowPower.h"
-#include "SPI.h"
-#include "RF24.h"
+#include <Arduino.h>
+
+#include <LowPower.h>
+
+#include <SPI.h>
+#include <Wire.h>
+
+#include <RF24.h>
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+
+#define SEALEVELPRESSURE_HPA (1013.25)
 
 /****************** User Config ***************************/
 /***      Set this radio as radio number 0 or 1         ***/
 bool radioNumber = 0;
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7,8);
+RF24 radio(7, 8);
 /**********************************************************/
 
-byte addresses[][6] = {"1Node","2Node"};
+/* BME280 Sensor by Adafruit */
+Adafruit_BME280 bme; // I2C
+
+byte addresses[][6] = {"1Node", "2Node"};
 
 // Used to control whether this node is sending or receiving
 bool role = 1;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
+
+  if (!bme.begin(0x77, &Wire))
+  {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1)
+      ;
+  }
+
   Serial.println(F("RF24/examples/GettingStarted"));
   Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
-  
+
   radio.begin();
 
   // Set the PA Level low to prevent power supply related issues since this is a
- // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
   radio.setPALevel(RF24_PA_LOW);
-  
+
   // Open a writing and reading pipe on each radio, with opposite addresses
-  if(radioNumber){
+  if (radioNumber)
+  {
     radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
-  }else{
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
+    radio.openReadingPipe(1, addresses[0]);
   }
-  
+  else
+  {
+    radio.openWritingPipe(addresses[0]);
+    radio.openReadingPipe(1, addresses[1]);
+  }
+
   // Start the radio listening for data
   radio.startListening();
 }
 
-void loop() {
-  
-  
-/****************** Ping Out Role ***************************/  
-if (role == 1)  {
-    
-    radio.stopListening();                                    // First, stop listening so we can talk.
-    
-    
+void printValues()
+{
+  Serial.print("Temperature = ");
+  Serial.print(bme.readTemperature());
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.print("Humidity = ");
+  Serial.print(bme.readHumidity());
+  Serial.println(" %");
+
+  Serial.println();
+}
+
+void loop()
+{
+
+  /****************** Ping Out Role ***************************/
+  if (role == 1)
+  {
+
+    radio.stopListening(); // First, stop listening so we can talk.
+
     Serial.println(F("Now sending"));
 
-    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
-     if (!radio.write( &start_time, sizeof(unsigned long) )){
-       Serial.println(F("failed"));
-     }
-        
-    radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
+    unsigned long start_time = micros(); // Take the time, and send it.  This will block until complete
+    if (!radio.write(&start_time, sizeof(unsigned long)))
+    {
+      Serial.println(F("failed"));
     }
-        
-    if ( timeout ){                                             // Describe the results
-        Serial.println(F("Failed, response timed out."));
-    }else{
-        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &got_time, sizeof(unsigned long) );
-        unsigned long end_time = micros();
-        
-        // Spew it
-        Serial.print(F("Sent "));
-        Serial.print(start_time);
-        Serial.print(F(", Got response "));
-        Serial.print(got_time);
-        Serial.print(F(", Round-trip delay "));
-        Serial.print(end_time-start_time);
-        Serial.println(F(" microseconds"));
+
+    radio.startListening(); // Now, continue listening
+
+    unsigned long started_waiting_at = micros(); // Set up a timeout period, get the current microseconds
+    boolean timeout = false;                     // Set up a variable to indicate if a response was received or not
+
+    while (!radio.available())
+    { // While nothing is received
+      if (micros() - started_waiting_at > 200000)
+      { // If waited longer than 200ms, indicate timeout and exit while loop
+        timeout = true;
+        break;
+      }
+    }
+
+    if (timeout)
+    { // Describe the results
+      Serial.println(F("Failed, response timed out."));
+    }
+    else
+    {
+      unsigned long got_time; // Grab the response, compare, and send to debugging spew
+      radio.read(&got_time, sizeof(unsigned long));
+      unsigned long end_time = micros();
+
+      // Spew it
+      Serial.print(F("Sent "));
+      Serial.print(start_time);
+      Serial.print(F(", Got response "));
+      Serial.print(got_time);
+      Serial.print(F(", Round-trip delay "));
+      Serial.print(end_time - start_time);
+      Serial.println(F(" microseconds"));
     }
 
     // Try again 1s later
 
     //delay(1000);
-    delay(100);  // otherwise crappy terminal output
-    LowPower.idle(SLEEP_2S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
-                SPI_OFF, USART0_OFF, TWI_OFF);
+    delay(100); // otherwise crappy terminal output
+    LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF,
+                  SPI_OFF, USART0_OFF, TWI_OFF);
     //            SPI_OFF USART0_OFF, TWI_OFF);
   }
 
+  /****************** Pong Back Role ***************************/
 
-
-/****************** Pong Back Role ***************************/
-
-  if ( role == 0 )
+  if (role == 0)
   {
     unsigned long got_time;
-    
-    if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
+
+    if (radio.available())
+    {
+      // Variable for the received timestamp
+      while (radio.available())
+      {                                               // While there is data ready
+        radio.read(&got_time, sizeof(unsigned long)); // Get the payload
       }
-     
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
+
+      radio.stopListening();                         // First, stop listening so we can talk
+      radio.write(&got_time, sizeof(unsigned long)); // Send the final one back.
+      radio.startListening();                        // Now, resume listening so we catch the next packets.
       Serial.print(F("Sent response "));
-      Serial.println(got_time);  
-   }
- }
-
-
-
-
-/****************** Change Roles via Serial Commands ***************************/
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-      role = 1;                  // Become the primary transmitter (ping out)
-    
-   }else
-    if ( c == 'R' && role == 1 ){
-      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
-       role = 0;                // Become the primary receiver (pong back)
-       radio.startListening();
-       
+      Serial.println(got_time);
     }
   }
 
+  /****************** Change Roles via Serial Commands ***************************/
+
+  if (Serial.available())
+  {
+    char c = toupper(Serial.read());
+    if (c == 'T' && role == 0)
+    {
+      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
+      role = 1; // Become the primary transmitter (ping out)
+    }
+    else if (c == 'R' && role == 1)
+    {
+      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));
+      role = 0; // Become the primary receiver (pong back)
+      radio.startListening();
+    }
+  }
+
+  bme.takeForcedMeasurement(); // has no effect in normal mode
+
+  printValues();
 
 } // Loop
